34ad62d6d3d201f3a78884ca859a422add3ea3b3
Adam Moore <amooreinteractive@gmail.com>
2014-10-04 15:41:31 -0700

    extra energy handled

--- Messages.py
+++ Messages.py
@@ -33,11 +33,22 @@ class messages:
         screen.blit(self.rateLabel, self.rateRect)
         
         for i in range(1,int(ship.energy)+1):
-            if i%10 == 0 or i%10 == 1:
-                pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y+3), ((self.energyBar.x+i*2),self.energyBar.y+22))
-            
-            elif i%10 == 9 or i%10 == 2:
-                pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y+1), ((self.energyBar.x+i*2),self.energyBar.y+24))
-            
+            if i > 100:
+                i1 = i-100
+                if i1%10 == 0 or i1%10 == 1:
+                    pygame.draw.line(screen, (85, 235, 250), ((self.energyBar.x+i1*2),self.energyBar.y+15), ((self.energyBar.x+i1*2),self.energyBar.y+22))
+                
+                elif i1%10 == 9 or i1%10 == 2:
+                    pygame.draw.line(screen, (85, 235, 250), ((self.energyBar.x+i1*2),self.energyBar.y+15), ((self.energyBar.x+i1*2),self.energyBar.y+24))
+                
+                else:
+                    pygame.draw.line(screen, (85, 235, 250), ((self.energyBar.x+i1*2),self.energyBar.y+15), ((self.energyBar.x+i1*2),self.energyBar.y+25))
             else:
-                pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y), ((self.energyBar.x+i*2),self.energyBar.y+25))
+                if i%10 == 0 or i%10 == 1:
+                    pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y+3), ((self.energyBar.x+i*2),self.energyBar.y+22))
+                
+                elif i%10 == 9 or i%10 == 2:
+                    pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y+1), ((self.energyBar.x+i*2),self.energyBar.y+24))
+                
+                else:
+                    pygame.draw.line(screen, (55, 135, 250), ((self.energyBar.x+i*2),self.energyBar.y), ((self.energyBar.x+i*2),self.energyBar.y+25))
