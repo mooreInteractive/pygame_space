c0cb9345a432341ed4a09cc394152fc4220eb7b9
Adam Moore <amooreinteractive@gmail.com>
2014-10-06 22:54:35 -0700

    small bug

--- space.py
+++ space.py
@@ -172,7 +172,7 @@ def detectCollisions():
                 mainMenu.__init__(block)
                 gamestate = 'menu'
             enemies.remove(n)
-            breaks
+            break
         for h in block.hull[:]:
                 #print hullCount
                 thisHull = pygame.Rect(h.x, h.y, h.w, h.h)
